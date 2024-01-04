6946ce90bab9b0d0046cc5a017ac00f116dbeef4
fastweb-kr <support@fast-web.kr>
2024-01-04 12:26:45 +0900

    fix. FAB 위치 수정

--- src/features/ui/Button/Fab/Fab.tsx
+++ src/features/ui/Button/Fab/Fab.tsx
@@ -1,17 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 import * as S from './Fab.styled';
 
-const Fab = () => {
+interface FabProps {
+  positionX: number;
+}
+
+const GLOBAL_PADDING = 16;
+
+const Fab: React.FC<FabProps> = ({ positionX }) => {
   const navigate = useNavigate();
 
   const handleWrite = () => {
     navigate('/style/write');
   };
 
+  const fabPosition = {
+    right: positionX + GLOBAL_PADDING,
+  };
+
   return (
     <>
       <S.Container>
-        <S.Button onClick={handleWrite}>+</S.Button>
+        <S.Button onClick={handleWrite} style={fabPosition}>
+          +
+        </S.Button>
       </S.Container>
     </>
   );
--- src/pages/Style/StyleDetail/StyleDetail.styled.ts
+++ src/pages/Style/StyleDetail/StyleDetail.styled.ts
@@ -2,12 +2,31 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const UserSection = styled.section`
+export const Section = styled.section`
+  padding: 0 var(--global-padding);
+  margin-bottom: 32px;
+`;
+
+export const Figure = styled.figure`
+  width: 48px;
+  height: 48px;
+  background-color: var(--color-gray-01);
+  border-radius: 30px;
+  overflow: hidden;
+`;
+
+export const Avatar = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const UserSection = styled(Section)`
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  padding: 0 var(--global-padding);
+  margin-bottom: 8px;
 `;
 
 export const UserWrap = styled.div`
@@ -16,19 +35,7 @@ export const UserWrap = styled.div`
   gap: 8px;
 `;
 
-export const UserAvatarFigure = styled.figure`
-  width: 48px;
-  height: 48px;
-  background-color: var(--color-gray-01);
-  border-radius: 30px;
-  overflow: hidden;
-`;
-
-export const UserAvatar = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
+export const UserAvatarFigure = styled(Figure)``;
 
 export const UserProfileWrap = styled.div`
   display: flex;
@@ -62,4 +69,120 @@ export const FollowButton = styled.button`
   font-weight: 500;
   border: none;
   border-radius: 4px;
+`;
+
+export const ImageSection = styled.div`
+  width: 100%;
+  height: 500px;
+  background-color: var(--color-gray-01);
+`;
+
+export const IconSection = styled(Section)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px var(--global-padding);
+  margin-bottom: 0;
+`;
+
+export const LeftIconWrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const RightIconWrap = styled.div``;
+
+export const IconButton = styled.div`
+  display: inline-block;
+
+  & img {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+export const ContentSection = styled(Section)``;
+
+export const SectionTitle = styled.p`
+  font-size: 14px;
+  line-height: 14px;
+  font-weight: 400;
+  color: rgb(187, 187, 187);
+  margin-bottom: 4px;
+`;
+
+export const Time = styled(SectionTitle)``;
+
+export const ContentWrap = styled.div`
+  width: 100%;
+  vertical-align: baseline;
+
+  & ${UserName} {
+    display: inline;
+    margin-right: 4px;
+  }
+`;
+
+export const Content = styled.p`
+  display: inline;
+  font-size: 14px;
+  line-height: 1.4;
+`;
+
+export const ProductSection = styled(Section)``;
+
+export const ProductSectionTitle = styled(SectionTitle).attrs({ as: 'h3' })``;
+
+export const TagSection = styled(Section)``;
+
+export const TagSectionTitle = styled(SectionTitle).attrs({ as: 'h4' })`
+  margin-bottom: 8px;
+`;
+
+export const TagList = styled.ul`
+  display: flex;
+  gap: 8px;
+  flex-wrap: wrap;
+`;
+
+export const TagItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 31px;
+  padding: 8px 14px 7px;
+  border-radius: 16.5px;
+  border: 1px solid rgb(228, 228, 228);
+  font-size: 13px;
+  font-weight: 500;
+  background-color: var(--color-wh);
+  color: var(--coltor-bk);
+`;
+
+export const CommentSection = styled(Section)``;
+
+export const CommentSectionTitle = styled(SectionTitle).attrs({ as: 'button' })`
+  margin-bottom: 8px;
+`;
+
+export const CommentWrap = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const CommentAvatarFigure = styled(Figure)`
+  width: 32px;
+  height: 32px;
+`;
+
+export const InputText = styled.input`
+  width: 100%;
+  border: 1px solid var(--color-gray-03);
+  padding: 0 8px;
+  border-radius: 4px;
+
+  &::placeholder {
+    color: var(--color-gray-03);
+  }
 `;
